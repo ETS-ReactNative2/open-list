408391658547b1c40fced46c094bcc6232373ba2
amir248 <chikchicly@gmail.com>
2022-05-09 12:30:37 +0700

    newBranch

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './header.css';
+
+class Header extends Component {
+  render() {
+    return (
+    <React.Fragment>
+    <h1>Карты</h1>
+    </React.Fragment>
+    );
+  }
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './header';
+import './index.css';
+
+ReactDOM.render(
+  <Header />,
+  document.querySelector('header')
+);
