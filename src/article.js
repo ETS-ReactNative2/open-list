761b3c1b8b56f37a4528721fe4ad32793ac94e8b
amir248 <chikchicly@gmail.com>
2022-05-09 12:55:23 +0700

    npm_run_build

---
+++ src/article.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './header.css';
+
+class Article extends Component {
+  render() {
+    return (
+    <React.Fragment>
+    <h1>Article</h1>
+    </React.Fragment>
+    );
+  }
+}
+
+export default Article;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import Header from './header';
 import './index.css';
 
+import Article from './article';
+
 ReactDOM.render(
   <Header />,
   document.querySelector('header')
+);
+
+ReactDOM.render(
+  <Article/>,
+  document.querySelector('article')
 );
